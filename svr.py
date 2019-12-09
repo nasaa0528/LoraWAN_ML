5b6d55cd556e7a7446dbac2830ac6b935769b2ed
nasaa0528 <minerva2991@gmail.com>
2019-12-09 20:36:42 +0100

    Added DeepNN and argparse for dynamic training

--- svr.py
+++ svr.py
@@ -1,3 +1,4 @@
+import argparse
 import torch 
 from torch.autograd import Variable 
 from torch.nn import functional as F 
@@ -9,26 +10,38 @@ import matplotlib.pyplot as plt
 torch.set_printoptions(precision=1)
 
 
-from models import LinearRegression
+from models import *
 from dataset import PowerDataset
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--epoch", type=int, default=200, help="total num of epochs")
+parser.add_argument("--batch_size", type=int, default=6, help="size of the batches")
+parser.add_argument("--lr", type=float, default=0.01, help="SGD: learning rate")
+parser.add_argument("--input_size", type=int, default=15, help="number of input features")
+parser.add_argument("--output_size", type=int, default=10, help="number of output features")
+parser.add_argument("--model_type", type=str, default="deepnn", help="type of model to use")
+parser.add_argument("--data_file", type=str, default='Data_set_no_formula.xlsx', help="type of model to use")
+opt = parser.parse_args()
 
-model = LinearRegression()
+if opt.model_type == 'svr':
+	model = SupportVectorRegression(opt.input_size, opt.output_size)
+elif opt.model_type == 'deepnn':
+	model = DeepNN(hidden_size=100, input_size=opt.input_size, output_size=opt.output_size)
 criterion = torch.nn.MSELoss(reduction='sum')
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+optimizer = torch.optim.SGD(model.parameters(), lr=opt.lr)
 
 model = model.float() 
 
 # Training data loader
 dataloader = DataLoader(
-    PowerDataset('Data_set_no_formula.xlsx'),
-    batch_size=6,
+    PowerDataset(opt.data_file),
+    batch_size=opt.batch_size,
     shuffle=True,
 )
 
 # Test data loader
 val_dataloader = DataLoader(
-	PowerDataset(path='Data_set_no_formula.xlsx', mode="test"),
+	PowerDataset(path=opt.data_file, mode="test"),
     batch_size=5,
     shuffle=True,
     # num_workers=1,
@@ -37,7 +50,7 @@ val_dataloader = DataLoader(
 
 #### Trainging ####
 running_loss = 0.0
-for epoch in range(1):
+for epoch in range(opt.epoch):
 	for i, batch in enumerate(dataloader):
 		y_val = batch['y_val']
 		x_val = batch['x_val']
@@ -67,17 +80,16 @@ print('\nFinished Training')
 length_data = len(val_dataloader)
 print(length_data)
 model.eval()
-for epoch in range(1):
-	for i, batch in enumerate(val_dataloader):
-		y_val_test = batch['y_val']
-		x_val_test = batch['x_val']
+for i, batch in enumerate(val_dataloader):
+	y_val_test = batch['y_val']
+	x_val_test = batch['x_val']
 
-		y_pred_test = model(x_val_test.float())
-		# print("Real Values: \t",  y_val_test)
-		# print("Predicted Values: \t",  y_val_test)
-		if i == 0: 
-			result = (abs(y_pred_test - y_val_test)).data.numpy()
-		print("Absolute Difference:\t", (abs(y_pred_test - y_val_test)).data.numpy())
+	y_pred_test = model(x_val_test.float())
+	# print("Real Values: \t",  y_val_test)
+	# print("Predicted Values: \t",  y_val_test)
+	if i == 0: 
+		result = (abs(y_pred_test - y_val_test)).data.numpy()
+	print("Absolute Difference:\t", (abs(y_pred_test - y_val_test)).data.numpy())
 # print(result.shape)
 # new_xticks = ['Tsym ms', 
 # 			'PayloadSymbNb ms', 
